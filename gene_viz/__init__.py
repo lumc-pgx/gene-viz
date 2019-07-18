85627b47d2b69b6b3912fa9c06e9cd0733dbc567
Guy Allard <w.g.allard@lumc.nl>
2019-07-18 11:41:12 +0200

    updated author and copyright

--- gene_viz/__init__.py
+++ gene_viz/__init__.py
@@ -4,6 +4,6 @@ __version_info__ = ("0", "0", "1", "dev")
 __date__ = "09 Nov 2017"
 
 __version__ = ".".join(__version_info__)
-__author__ = "Guy Allard"
+__author__ = "Guy Allard, LUMC"
 __contact__ = "guyallard01@gmail.com"
-__homepage__ = "https://git.lumc.nl/VisualizationTools/gene_viz"
+__homepage__ = "https://github.com/lumc-pgx/gene-viz"
